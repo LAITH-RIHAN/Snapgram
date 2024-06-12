bb59d4a121ca3785128b25f31d2c072d6eeac11a
LAITH-RIHAN <135272940+LAITH-RIHAN@users.noreply.github.com>
2024-06-12 11:20:04 +0300

    basic structure
    installed tailwind shadcn and build the main structure

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import vitePluginRequire from "vite-plugin-require";
+import path from "path"
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(),vitePluginRequire(),],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
